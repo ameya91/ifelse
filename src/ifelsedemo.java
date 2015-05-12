ab6295ffd7f299e5f46cc93f40f7637ad6127415
Ameya <ameyadighe91@gmail.com>
2015-05-12 17:45:49 +0530

    second commit

---
+++ src/ifelsedemo.java
@@ -0,0 +1,14 @@
+
+public class ifelsedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=7;
+if (num>5){
+	System.out.println("Success");}
+else if (num<5){
+	System.out.println("Failure");}
+	
+	System.out.println("Executed successfully");
+}
+}
